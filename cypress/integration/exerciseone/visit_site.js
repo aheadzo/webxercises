f7e475998a2641b69b424db076bc519266c1ffe6
Navarro <uid42223@contiwan.com>
2020-03-31 16:21:08 -0600

    Showing the info of datatable click

--- cypress/integration/exerciseone/visit_site.js
+++ cypress/integration/exerciseone/visit_site.js
@@ -1,6 +1,6 @@
-describe('test_name', function() {
+describe('Type on search element and click to display datatable information', function() {
 
- it('what_it_does', function() {
+ it('Click, type and search, then display', function() {
 
     cy.viewport(1536, 731);
  
@@ -14,7 +14,10 @@ describe('test_name', function() {
     cy.contains('Quick Find').find('.slds-input').should( ($input) => {
         //assert the text type
         expect($input).to.have.value('datatable');
-    })
+    });
+
+    //click on text after the search
+    cy.get('componentreference-sidebar').contains('datatable').find('.slds-truncate').contains('datatable').click({force: true});
      
  });
 
