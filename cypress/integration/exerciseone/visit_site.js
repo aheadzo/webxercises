a7552df4890a3cae7e5f85cda4eed05983dc9ffc
Navarro <uid42223@contiwan.com>
2020-03-31 11:12:11 -0600

    After some changes, the search shadow DOM element can be typed

--- cypress/integration/exerciseone/visit_site.js
+++ cypress/integration/exerciseone/visit_site.js
@@ -7,17 +7,13 @@ describe('test_name', function() {
     cy.visit('https://developer.salesforce.com/docs/component-library/documentation/en/lwc');
     
     cy.visit('https://developer.salesforce.com/docs/component-library/overview/components');
+
+    cy.contains('Quick Find').find('.slds-input').type('datatable', {force: true} );
     
     cy.contains('Quick Find').find('.slds-input').should( ($input) => {
-        //assert the empty input
-        expect($input).to.have.value('');
-    }).then(($input) => {
-        //edit the input value
-        const [dom] = ($input).get();
-        dom.value='datatable';
         //assert the text type
         expect($input).to.have.value('datatable');
-    });
+    })
      
  });
 
