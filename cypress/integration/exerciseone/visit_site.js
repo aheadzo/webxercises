937cf0472e51b6be6daa87e8ffc3fe150f430141
Navarro <uid42223@contiwan.com>
2020-03-31 19:31:13 -0600

    Fixed click on Component Reference, adding .gitignore

--- cypress/integration/exerciseone/visit_site.js
+++ cypress/integration/exerciseone/visit_site.js
@@ -1,14 +1,17 @@
-describe('Type on search element and click to display datatable information', function() {
+describe('Automation Project for Salesforce LightwebComponents', function() {
 
  it('Click, type and search, then display', function() {
 
     cy.viewport(1536, 731);
  
+    //Navigate to the link: https://developer.salesforce.com/docs/component-library/documentation/en/48.0/lwc
     cy.visit('https://developer.salesforce.com/docs/component-library/documentation/en/lwc');
     
-    cy.visit('https://developer.salesforce.com/docs/component-library/overview/components');
+    
+    //Switch to the Component Reference tab
+    cy.contains('Component Reference').find('.slds-truncate').contains('Component Reference').click({force: true});
 
-    //typing on search element
+    //Search in Quick Find for “datatable” 
     cy.contains('Quick Find').find('.slds-input').type('datatable', {force: true} );
     
     cy.contains('Quick Find').find('.slds-input').should( ($input) => {
@@ -16,9 +19,27 @@ describe('Type on search element and click to display datatable information', fu
         expect($input).to.have.value('datatable');
     });
 
-    //click on text after the search
+    //Under Lightning Web Components, click on the Components>lightning>“datatable” on the left menu panel
     cy.get('componentreference-sidebar').contains('datatable').find('.slds-truncate').contains('datatable').click({force: true});
+
+    //select Datatable from Inline Edit for the dropdown
      
  });
 
 });
+
+/*describe('test_name', function() {
+
+    it('what_it_does', function() {
+   
+       cy.viewport(1031, 731)
+    
+       cy.visit('https://developer.salesforce.com/docs/component-library/bundle/lightning-datatable/example')
+    
+       cy.get('.slds-form-element__control > .slds-combobox_container > .slds-combobox > .slds-combobox__form-element > #input-887').click()
+    
+       cy.get('.slds-combobox_container > .slds-combobox > #dropdown-element-887 > #input-887-3-887 > .slds-media__body').click()
+    
+    })
+   
+   })*/
